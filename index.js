c06bf03af85dd7440a6fc3a41a1c867254dd2ca7
Eli Wood <mreliwood1@gmail.com>
2022-04-25 17:15:44 -0700

    working program

--- index.js
+++ index.js
@@ -1,121 +1,17 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const team = require('./src/generateHtml');
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 const newTeam = [];
 
-const newEmployee = () => {
-
-    inquirer
-        .prompt([
-
-            {
-                type: 'list',
-                message: 'What type of team member are you adding?',
-                choices: ['Manager', 'Engineer', 'Intern'],
-                name: 'role',
-            },
-            {
-                type: 'input',
-                message: 'What is their name?',
-                name: 'name',
-            },
-            {
-                type: 'input',
-                message: 'What is their employee ID number?',
-                name: 'id',
-            },
-            {
-                type: 'input',
-                message: 'What is their email?',
-                name: 'email',
-            },
-
-        ])
-        .then(function (response) {
-
-            let { role, name, id, email } = response;
-
-            if (role === 'Manager') {
-
-                inquirer
-                    .prompt([
-
-                        {
-                            type: 'input',
-                            message: 'What is their office number?',
-                            name: 'officeNumber'
-                        },
-                        {
-                            type: 'list',
-                            message: 'Would you like to add another team member?',
-                            choices: ['Yes', 'No'],
-                            name: 'school'
-                        }
-
-                    ])
-                    .then(function (response) {
-                        let { officeNumber, addMore } = response;
-
-                        const newManager = new Manager(role, name, id, email, officeNumber);
-
-                        newTeam.push(newManager);
-
-                        if (addMore === 'Yes') {
-
-                            teamBuilder();
-
-                        };
-
-                    });
-
-            } else if (role === 'Engineer') {
-
-                inquirer
-                    .prompt([
-
-                        {
-                            type: 'input',
-                            message: 'What is their GitHub username?',
-                            name: 'github'
-                        }
-
-                    ])
-                    .then(function (response) {
-                        let { github } = response;
-                    });
-
-            } else if (role === 'Intern') {
-
-                inquirer
-                    .prompt([
-
-                        {
-                            type: 'input',
-                            message: 'What school are they attending or most recently attended?',
-                            name: 'school'
-                        }
-
-                    ])
-                    .then(function (response) {
-                        let { school } = response;
-                    });
-
-            }
-
-        });
-
-};
-
 const generateHtml = () => {
-
-    console.log('New Team:' + newTeam);
 
     const generatedHtml = team(newTeam);
 
-    console.log('HTML:' + generatedHtml);
+    newTeam.join("");
 
     fs.writeFile('dist/team.html', generatedHtml, function (error) {
         if (error) { console.error(error) };
@@ -170,7 +66,7 @@ const teamBuilder = () => {
                             type: 'list',
                             message: 'Would you like to add another team member?',
                             choices: ['Yes', 'No'],
-                            name: 'school'
+                            name: 'addMore'
                         }
 
                     ])
@@ -189,7 +85,7 @@ const teamBuilder = () => {
 
                             generateHtml();
 
-                        }
+                        };
 
                     });
 
@@ -202,11 +98,32 @@ const teamBuilder = () => {
                             type: 'input',
                             message: 'What is their GitHub username?',
                             name: 'github'
+                        },
+                        {
+                            type: 'list',
+                            message: 'Would you like to add another team member?',
+                            choices: ['Yes', 'No'],
+                            name: 'addMore'
                         }
 
                     ])
                     .then(function (response) {
-                        let { github } = response;
+                        let { github, addMore } = response;
+
+                        const newEngineer = new Engineer(role, name, id, email, github);
+
+                        newTeam.push(newEngineer);
+
+                        if (addMore === 'Yes') {
+
+                            teamBuilder();
+
+                        } else {
+
+                            generateHtml();
+
+                        };
+
                     });
 
             } else if (role === 'Intern') {
@@ -218,11 +135,32 @@ const teamBuilder = () => {
                             type: 'input',
                             message: 'What school are they attending or most recently attended?',
                             name: 'school'
+                        },
+                        {
+                            type: 'list',
+                            message: 'Would you like to add another team member?',
+                            choices: ['Yes', 'No'],
+                            name: 'addMore'
                         }
 
                     ])
                     .then(function (response) {
-                        let { school } = response;
+                        let { school, addMore } = response;
+
+                        const newIntern = new Intern(role, name, id, email, school);
+
+                        newTeam.push(newIntern);
+
+                        if (addMore === 'Yes') {
+
+                            teamBuilder();
+
+                        } else {
+
+                            generateHtml();
+
+                        };
+
                     });
 
             }
